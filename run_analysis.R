19dc75253893ecb4650ca303c599003863956d90
sidv1993 <sidv1993@gmail.com>
2016-04-18 02:28:31 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,6 @@ setwd("~/DS Coursera/Getting and Cleaning Data/Week4 Course Project/UCI HAR Data
 library(plyr)
 
 ## Load the datasets
-
 train_x <- read.table("train/X_train.txt")
 train_y <- read.table("train/y_train.txt")
 train_sub <- read.table("train/subject_train.txt")
@@ -10,13 +9,16 @@ test_x <- read.table("test/X_test.txt")
 test_y <- read.table("test/y_test.txt")
 test_sub <- read.table("test/subject_test.txt")
 
+## Load features and activity datasets
 features <- read.table("features.txt", colClasses = c("character"))
 activity_labels <- read.table("activity_labels.txt", col.names = c("ActivityId", "Activity"))
 
-############ 1.Merge the training and the test sets to create one data set.
+############ 1.Merge the training and the test sets to create one data set
 
 train <- cbind(train_y, train_sub, train_x)
 test <- cbind(test_y, test_sub, test_x)
+
+## Final dataset
 final_data <- rbind(train, test)
 
 ## Label the columns
@@ -42,11 +44,11 @@ names(final_data_mean_std) <- make.names(names(final_data_mean_std))
 
 # Make clearer names
 names(final_data_mean_std) <- gsub('Acc',"Acceleration", names(final_data_mean_std))
-names(final_data_mean_std) <- gsub('GyroJerk',"AngularAcceleration", names(final_data_mean_std))
-names(final_data_mean_std) <- gsub('Gyro',"AngularSpeed", names(final_data_mean_std))
+names(final_data_mean_std) <- gsub('GyroJerk',"Angular Acceleration", names(final_data_mean_std))
+names(final_data_mean_std) <- gsub('Gyro',"Angular Speed", names(final_data_mean_std))
 names(final_data_mean_std) <- gsub('Mag',"Magnitude", names(final_data_mean_std))
-names(final_data_mean_std) <- gsub('^t',"TimeDomain.", names(final_data_mean_std))
-names(final_data_mean_std) <- gsub('^f',"FrequencyDomain.", names(final_data_mean_std))
+names(final_data_mean_std) <- gsub('^t',"Time Domain.", names(final_data_mean_std))
+names(final_data_mean_std) <- gsub('^f',"Frequency Domain.", names(final_data_mean_std))
 names(final_data_mean_std) <- gsub('\\.mean',".Mean", names(final_data_mean_std))
 names(final_data_mean_std) <- gsub('\\.std',".Standard Deviation", names(final_data_mean_std))
 names(final_data_mean_std) <- gsub('Freq\\.',"Frequency.", names(final_data_mean_std))
